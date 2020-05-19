db5e1783ee120386d30f3e53649538d5dba5c9c0
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-19 19:13:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   arr = []
-  array.each { |i| arr << i^2}
-    i*i
-  end
+  array.each { |i| arr << i^2 }
+  arr
 end
+
+  arr = [1, 2]
+  square_array(Array.new(len,val)
+  )
